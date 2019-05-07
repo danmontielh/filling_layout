93e59fe2318f3cfc0e691b2873c37a0aaadb999e
Dan Montiel <danmontielh@gmail.com>
2019-05-07 18:33:35 -0500

    added login basic logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :users
   root 'static_pages#home'
 
@@ -9,4 +10,7 @@ Rails.application.routes.draw do
 
   get  '/signup',  to: 'users#new'
   post  '/signup',  to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
