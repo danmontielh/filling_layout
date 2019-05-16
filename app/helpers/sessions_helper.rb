3ce7923a5211baf34d263cd9daed176e29131336
Dan Montiel <danmontielh@gmail.com>
2019-05-16 18:22:54 -0500

    more bush solved

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,7 +21,7 @@ module SessionsHelper
        elsif (user_id = cookies.signed[:user_id])
             user = User.find_by(id: user_id)
             if user && user.authenticated?(:remember, cookies[:remember_token])
-                log_in
+                log_in user
                 @current_user = user
             end
         end
