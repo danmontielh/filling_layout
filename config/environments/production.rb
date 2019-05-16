a1eaba46b064ac750d68263fc3ef75311ff0a4cb
Dan Montiel <danmontielh@gmail.com>
2019-05-16 18:01:17 -0500

    fixed bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'secure-thicket-67993.herokuapp.com',
     :enable_starttls_auto => true
   }
 
